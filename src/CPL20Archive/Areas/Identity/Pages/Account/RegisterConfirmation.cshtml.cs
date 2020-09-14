6e06d3bd389fc51d83d90e5cb183bbca4218b4d4
Chad Green <chadgreen@chadgreen.com>
2020-09-13 20:32:02 -0400

    Removed the ability for someone to register for the site.

---
+++ src/CPL20Archive/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CPL20Archive.Areas.Identity.Pages.Account
+{
+	[AllowAnonymous]
+	public class RegisterConfirmationModel : PageModel
+	{
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+	}
+}
